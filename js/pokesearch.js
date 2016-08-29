5f3b09ab8d4a753543eb6e4d6e5c190add2f65db
Coding <coding@JobTrainworks.org>
2016-08-29 15:09:57 -0700

    Add append functionality

--- js/pokesearch.js
+++ js/pokesearch.js
@@ -12,9 +12,11 @@ $('.type').on('click', function(event){
     success: function(result){
       $results.empty();
       pNameArr = [];
+      console.log(result);
       for (var i = 0 ; i < result.pokemon.length ; i++){
         var pName = result.pokemon[i].pokemon.name;
         pNameArr.push(pName);
+        $results.append(($('<div>').addClass('info-card')).append(($('<div>').addClass('info-name'))).append($('<p>').text(pName)));
       }
       console.log(pNameArr);
     }
